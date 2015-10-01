31cf9cc140c8c4c2184765a830314b5fcaa2fb26
petdakov <petdakov@abv.bg>
2015-10-01 23:16:38 +0300

    Home Work Sample
    Playing With GitHub

---
+++ 1.SortArrayOfNumbers/SortArrayOfNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class SortArrayOfNumbers
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            int[] array = Array.ConvertAll(input.Split(' '), int.Parse);
+            int holder;
+
+            for (int k = 0; k < array.Length; k++)
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        holder = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = holder;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", array));
+
+        }
+    }
+
